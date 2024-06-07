1fb1ecb571f7680ceb02fc843eb959b19b22d627
Alhai <aly.haidara@seira-formation.fr>
2024-06-07 11:22:11 +0200

    add video

--- src/app/modules/concept/concept.component.ts
+++ src/app/modules/concept/concept.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./concept.component.css']
 })
 export class ConceptComponent {
-
+  videoUrl = 'https://hx7cqa3okvatltnn.public.blob.vercel-storage.com/Montage-VFinale-y7AjeBvuZEzZ1VEbrSkFx8RbUprTHb.mp4';
 }
