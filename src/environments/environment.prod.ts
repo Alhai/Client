a3e1824e688590fc092cfe3a26831370daaeed6c
Alhai <aly.haidara@seira-formation.fr>
2024-06-12 23:06:50 +0200

    maj

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     googleMapsApiKey: 'AIzaSyBRTN4mgmXbfLJ-w-22j8fHWdqBfqtW8_M',
-    apiUrl: 'http://206.189.17.29:3000',
+    apiUrl: 'https://206.189.17.29:3000',
 };
